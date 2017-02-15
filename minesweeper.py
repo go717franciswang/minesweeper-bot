1f4ce11b900850669e8b7b9007df1f75e077b0cb
Francis Wang <franciswang@Dune.local>
2017-02-15 11:20:14 -0800

    Solve beginner and intermediate board

--- minesweeper.py
+++ minesweeper.py
@@ -10,13 +10,33 @@ box_patterns = {
         '5': Pattern("Screen Shot 2017-02-09 at 5.50.30 PM.png").similar(0.90),
         '6': Pattern("Screen Shot 2017-02-09 at 5.52.50 PM.png").similar(0.90),
         '7': Pattern("Screen Shot 2017-02-09 at 5.55.42 PM.png").similar(0.90)}
-x = int(happy_face.getX()-448)
-y = int(happy_face.getY()+30)
+setting = {
+        'beginner': {
+            'x': int(happy_face.getX()-135),
+            'y': int(happy_face.getY()+30),
+            'box_w': 9,
+            'box_h': 9,
+            'board_regions': [Region(int(happy_face.getX()-135), int(happy_face.getY()+30), 270, 270)]
+            },
+        'intermediate': {
+            'x': int(happy_face.getX()-240),
+            'y': int(happy_face.getY()+30),
+            'box_w': 16,
+            'box_h': 16,
+            'board_regions': [Region(int(happy_face.getX()-240), int(happy_face.getY()+30), 480, 480)]
+            },
+        'advanced': {
+            'x': int(happy_face.getX()-450),
+            'y': int(happy_face.getY()+30),
+            'box_w': 30,
+            'box_h': 16,
+            'board_regions': [
+                Region(int(happy_face.getX()-450), int(happy_face.getY()+30), 450, 240),
+                Region(int(happy_face.getX()-450), int(happy_face.getY()+30)+240, 450, 240)]
+            }
+        }
+setting = setting['beginner']
 box_size = 30
-box_w = 30
-box_h = 16
-w = box_w*box_size
-h = box_h*box_size
 
 def match(region, pattern):
     try:
@@ -27,26 +47,25 @@ def match(region, pattern):
 has_move = True
 Settings.MoveMouseDelay = 0
 # split the board into segments
-h0 = box_h//2*box_size
-board_regions = [Region(x, y, w, h0), Region(x, y+h0, w, h-h0)]
+h0 = setting['box_h']//2*box_size
 def my_click(positions):
     for i in xrange(2):
         for r,c in positions:
-            click(Location(x+c*30+15, y+r*30+15))
+            click(Location(setting['x']+(c+0.5)*box_size, setting['y']+(r+0.5)*box_size))
 
 while has_move:
-    board = ("_"*30+"\n")*16
+    board = ("_"*setting['box_w']+"\n")*setting['box_h']
     print 'Analyzing patterns..'
     for k, pattern in box_patterns.iteritems():
-        for region in board_regions:
+        for region in setting['board_regions']:
             for m in match(region, pattern):
-                x0 = (m.getX()-x)//30
-                y0 = (m.getY()-y)//30
-                i = int(y0*31+x0)
+                x0 = (m.getX()-setting['x'])//box_size
+                y0 = (m.getY()-setting['y'])//box_size
+                i = int(y0*(setting['box_w']+1)+x0)
                 board = board[:i] + k + board[i+1:]
     print board
 
-    solver = Solver.from_string(board, 30, 16)
+    solver = Solver.from_string(board, setting['box_w'], setting['box_h'])
     solver.solve()
     print solver.to_string()
     print 'solved numbers: %s, solved bombs: %s' % (len(solver.revealed_numbers), len(solver.revealed_bombs))
